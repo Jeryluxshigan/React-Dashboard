c51aef4c31a87eac79a9e86b8341b1e0c2e32db6
Jeryluxshigan <jeryluxshiganp@fortude.co>
2024-02-27 11:11:10 +0530

    basic structure

---
+++ src/components/content/page1.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Page1() {
+    return (
+        <div className="content">
+          <div className="main-card">
+        
+          </div>
+        </div>
+      );
+
+}
+
+export default Page1
